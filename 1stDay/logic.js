46eae8124c6b656593324f996087299004360c6c
nikepludash <nikepludash@gmail.com>
2019-12-14 20:13:55 +0530

    Added LocalSession

--- 1stDay/logic.js
+++ 1stDay/logic.js
@@ -1,45 +1,49 @@
+function bodymaker(text) {
+    var div = document.createElement("div")
+    var input = document.createElement("input")
+    var label = document.createElement("label")
+    div.className = "row form-check ml-1"
+    label.htmlFor = "c" + (++i)
+    label.innerText = text
+    label.className = "form-check-label"
+    input.type = "checkbox"
+    input.className = "form-check-input"
+    input.id = "c" + i
+    input.setAttribute('data-index', i)
+    input.addEventListener("click", (e) => {
+        if (e.shiftKey && recent != null) {
+            checkboxes = document.querySelectorAll("input[type='checkbox']")
+            var lc = parseInt(recent.getAttribute("data-index"))
+            var cc = parseInt(e.srcElement.getAttribute("data-index"))
+            var low = 0, high = 0
+            lc > cc ? (low = cc, high = lc) : (low = lc, high = cc)
+            for (var k = low; k < high; k++) {
+                checkboxes[k].checked = recent.checked
+            }
+        }
+        recent = e.srcElement
+    })
+    div.appendChild(input)
+    div.appendChild(label)
+    return div
+}
+
 function handler() {
     let text = document.querySelector("#data")
     if (text.value.trim() != "") {
-        var div = document.createElement("div")
-        var input = document.createElement("input")
-        var label = document.createElement("label")
-        div.className="row form-check ml-1"
-        label.htmlFor = "c"+(++i)
-        label.innerText=text.value
-        label.className="form-check-label"
-        input.type="checkbox"
-        input.className="form-check-input"
-        input.id="c"+i
-        input.setAttribute('data-index', i)
-        input.addEventListener("click",(e)=>{
-            console.log(e.shiftKey,"pressed")
-            if (e.shiftKey && recent != null) {
-                console.log(recent,"press")
-                checkboxes = document.querySelectorAll("input[type='checkbox']")
-                var lc = parseInt(recent.getAttribute("data-index"))
-                var cc = parseInt(e.srcElement.getAttribute("data-index"))
-                console.log("lc="+lc,"cc="+cc)
-                var low=0 , high=0
-                lc > cc ? (low = cc, high = lc) : (low = lc, high = cc)
-                for (var k = low; k < high; k++) {
-                    checkboxes[k].checked=recent.checked
-                }
-            }
-            recent=e.srcElement
-        })
-        div.appendChild(input)
-        div.appendChild(label)
+        var div = bodymaker(text.value.trim())
+        localStorage.setItem("list:" + i, text.value.trim())
         body.appendChild(div)
+        localStorage.setItem("i", i)
     }
     else
         console.log("Empty")
     text.value = ""
-    if (!isThere) {document.getElementById("collapseOne").classList.add("show");isThere=true}
+    if (!isThere) { document.getElementById("collapseOne").classList.add("show"); isThere = true }
 }
 
-var i = 0,isThere=false
-var recent=null
+var i = 0, isThere = false
+var recent = null
 var body
 
 window.onload = () => {
@@ -49,5 +53,15 @@ window.onload = () => {
             handler()
         }
     })
-    
+    k = localStorage.getItem("i");
+    if (k) {
+        for (let j = 1; j <= k; j++) {
+            console.log(j)
+            var txt = localStorage.getItem("list:" + j)
+            var div = bodymaker(txt)
+            body.appendChild(div)
+        }
+    }
+    else i=0
+
 }
